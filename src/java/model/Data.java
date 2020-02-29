8911547d9bbef5d03057cb324c24409e448fd247
quynhvi <quynh.nguyen@mservice.com.vn>
2020-02-29 23:15:06 +0700

    WIP: convert html to pdf using itext render

---
+++ src/java/model/Data.java
@@ -0,0 +1,49 @@
+package model;
+
+public class Data {
+    private String firstname;
+    private String lastname;
+    private String street;
+    private String zipCode;
+    private String city;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
